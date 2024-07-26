447999a34ed72adb9a4474d18d94887d15202bb7
Patrick Hilmoe <pshilmoe@gmail.com>
2024-07-25 20:29:18 -0700

    First Commit

---
+++ README.md
@@ -0,0 +1,17 @@
+# URL shortening API challange from Front End Mentor
+
+The purpose of this site is to have a desktop and mobile friendly website with a responsive layout.
+
+
+Overall goals for the project
+- polished HTML and CSS layout
+    - pretty-ify any hinky areas
+        - make the cards laid out better
+    - update 'head' area info
+    - make it mobile friendly
+    - create polish the README
+
+- add to GitHub repos to add to portfolio
+
+- future goal: adding url api and local storage and adding layout for shortened urls
+    - wes bos might have a local storage resource to use.
---
+++ main.js
@@ -0,0 +1,43 @@
+import './style.css'
+
+document.querySelector('#app').innerHTML = `
+  <div>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <a class="navbar-brand" href="#">Shortly</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item">
+        <a class="nav-link" href="#">Features </a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Pricing</a>
+      </li>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Resources</a>
+      </li>
+    </ul>
+    <ul class="navbar-nav">
+      <liclass="nav-item">
+        <a > Login </a>
+      </li>
+      <lilass="nav-item">
+        <a class="nav-link"> Sign Up </a>
+      </li>
+    </ul>
+  </div>
+</nav>
+<div>
+  <p>More than just shorter links</p>
+  <p>Build your brand's recognition</p>
+  <button type="button" class="btn btn-success>Get Started</button>
+  <button type="button" class="btn btn-secondary">Secondary</button>
+</div>
+  </div>
+`
+
+
